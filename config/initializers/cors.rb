2d4956ddbdb701d48a4d95eab749e13bdfb65997
Aftab Sidhu <Aftab180@gmail.com>
2022-07-10 00:26:18 -0400

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000/'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-  allow do
-    origins 'https://classy-kashata-870e60.netlify.app/'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
