2b6ed34adbd0f022e428d1bedbebf67947290ae3
Aftab Sidhu <Aftab180@gmail.com>
2022-07-15 08:22:09 -0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,15 @@ Message.all.destroy_all
 Appwarning.all.destroy_all
 puts "🧹 cleaned successfully"
 bot = User.create({
-    email: 'bot@gmail.com',
+    email: 'bot@chat-app.com',
     password_digest: '123IAMBOT',
+    username: 'Chat App BOT'
 })
 puts "🤖 BOT initialized successfully"
-Message.create({user_id: bot.id, message: 'Welcome to Invite Me!'})
-#Group.create(user_id: bot.id, group_name: 'Public Chat', group_code: 'PUBLIC')
+botMessage = Message.create({user_id: bot.id, message: 'Welcome to Invite Me!'})
+firebase = Firebase::Client.new('https://invite-me-9a07f-default-rtdb.firebaseio.com')
+firebase.update('', {
+    'users/default' => bot,
+    'messages/default' => botMessage
+})
 puts "🌱 seeding successfull"
