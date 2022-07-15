6427605d81635fd37dfb3beb0ccc507a7f45144a
Aftab Sidhu <Aftab180@gmail.com>
2022-07-15 10:30:08 -0400

    update

--- app/controllers/appwarning_controller.rb
+++ app/controllers/appwarning_controller.rb
@@ -5,7 +5,7 @@ class AppwarningController < ApplicationController
         appwarning.count = 0
         appwarning.save
         firebase = Firebase::Client.new('https://invite-me-9a07f-default-rtdb.firebaseio.com')
-        bot = User.find_by(id: 1)
+        bot = User.find_by(email: 'bot@chat-app.com')
         botMessage = Message.create(user_id: bot.id, message: "#{User.find_by(id: params[:user_id]).username} Apologised for using inappropriate language.")
         firebase.push("messages", botMessage)
         renderObj = {
